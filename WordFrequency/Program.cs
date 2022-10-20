7323ddac7b1d391ee3fd3a3a88b434bb258a9d78
Novruz F. Aliyev <AliyevNF@kapitalho.az>
2022-10-20 18:04:18 +0400

    First commit

---
+++ WordFrequency/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+
+//Could be exported from http or file
+string sentence = "This is brand new sentence. This potato is very good brand.";
+
+sentence = sentence.ToLower();
+
+var words = Regex.Split(sentence, @"[^\p{L}]*\p{Z}[^\p{L}]*");
+
+Dictionary<string, int> wordFreqs = new Dictionary<string, int>();
+foreach (var word in words)
+{
+    wordFreqs.TryGetValue(word, out int frequency);
+    wordFreqs[word] = frequency + 1;
+}
+
+foreach (var pair in wordFreqs)
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
